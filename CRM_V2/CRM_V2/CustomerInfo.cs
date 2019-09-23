489e6c3c850e71bb8ef3cbf707e5cd1ff1a6d35e
Linus Gyllenberg <linus@sbgdata.se>
2019-09-23 13:09:10 +0200

    Initial commit

---
+++ CRM_V2/CRM_V2/CustomerInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_V2
+{
+    class CustomerInfo
+    {
+        public String Name { get; set; }
+        public string phonenumber { get; set; }
+        public string mail { get; set; }
+        public string favorit { get; set; }
+
+    }
+}
---
+++ CRM_V2/CRM_V2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.ArrayList CustomerInfo = new System.Collections.ArrayList();
+
+            bool cont = true;
+            String again;
+            String nstr;
+            String pstr;
+            String mstr;
+            String fstr;
+
+            while (cont == true)
+            {
+                Console.WriteLine("Ange namn");
+                nstr = Console.ReadLine();
+                Console.WriteLine("Ange telefonnummer");
+                pstr = Console.ReadLine();
+                Console.WriteLine("Ange mail");
+                mstr = Console.ReadLine();
+                Console.WriteLine("Ange favorit (Ja/Nej)");
+                fstr = Console.ReadLine();
+
+                CustomerInfo.Add(new CustomerInfo() { Name = nstr, phonenumber = pstr, mail = mstr, favorit = fstr });
+                Console.WriteLine("Tack, registrerad, mer? (Ja/Nej)");
+                again = Console.ReadLine();
+                if (again.Equals("Nej"))
+                {
+                    cont = false;
+                }
+
+            }
+
+            int favCount = 0;
+            Console.WriteLine("\nDu har {0} st kontakter", CustomerInfo.Count);
+            foreach (CustomerInfo tot in CustomerInfo)
+            {
+                Console.WriteLine("{0} {1} {2} {3}", tot.Name, tot.phonenumber, tot.mail, tot.favorit);
+                if (tot.favorit.Equals("Ja"))
+                {
+                    favCount++;
+                }
+            }
+            Console.WriteLine("\nDu har {0} st favoriter", favCount);
+            foreach (CustomerInfo tot in CustomerInfo)
+            {
+                if (tot.favorit.Equals("Ja"))
+                {
+                    Console.WriteLine("{0} {1} {2} {3}", tot.Name, tot.phonenumber, tot.mail, tot.favorit);
+                }
+            }
+        }
+    }
+}
